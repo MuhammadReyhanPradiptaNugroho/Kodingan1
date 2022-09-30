8c5dcd5cbd7c07c53671239e78bbe15df5253460
Muhammad Reyhan Pradipta Nugroho <114756485+MuhammadReyhanPradiptaNugroho@users.noreply.github.com>
2022-09-30 22:19:18 +0700

    Add files via upload

---
+++ Pembanding.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipedata;
+
+public class Pembanding {
+    public static void main(String[] args) {
+        int age = 36;
+        boolean hasilBanding1 = age < 25;
+        boolean hasilBanding2 = age != 26;
+        System.out.println("Tampilkan hasilBanding1"+hasilBanding1);
+        System.out.println("Tampilkan hasilBanding2"+hasilBanding2);
+                
+    }
+}
